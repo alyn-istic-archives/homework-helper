e7c38096ada1fc811357fbac2728f7ccdeb98158
Alyn <alynte3@gmail.com>
2026-04-07 13:07:52 -0400

    adding wave animation
    wave animation succesfully added! trying to work on drop down rn

--- course.js
+++ course.js
@@ -1,4 +1,6 @@
 const courseset = document.getElementById("coursebtns");
+let select = document.getElementById("dynamicDropdown");
+let courselist = [];
 
 function genCourseBtns() {
     if (!courseset) return;
@@ -27,7 +29,37 @@ function createCourse(coursename) {
         }
     });
 
+    courselist.push(coursename);
     courseset.appendChild(cbtn);
+    
+    updateDropdown();
 }
 
+
+function updateDropdown(set) {
+
+  // Clear existing options to prevent duplicates
+    select.innerHTML = '<option disabled selected>bro an option</option>';
+
+    if (set!=null){
+        for (let i = 0; i<set.length; i++){
+            let el = document.createElement("option"); 
+            el.textContent = "hullo"; // Set visible text
+            el.value = "pls work";       // Set value for form submission
+            select.appendChild(el);
+        };
+    }
+
+
+  // Loop through the array and append new options
+    // if (set){
+    //     set.forEach(course => {// Create <option> element
+    //         el.textContent = "pls bro"; // Set visible text
+    //         el.value = "pls work";       // Set value for form submission
+    //         select.appendChild(el); // Add to the dropdown
+    // });
+    // }
+}
+
+// Call the function whenever the array changes
 genCourseBtns();
